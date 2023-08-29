a3aaf376adf797099d678bbe624d916ccca50c16
Beatriz <beatriz.mediatto2@gmail.com>
2023-08-29 09:26:27 -0300

    feat: add service fase c

--- src/TrybeHotel/Services/TokenGenerator.cs
+++ src/TrybeHotel/Services/TokenGenerator.cs
@@ -13,20 +13,38 @@ namespace TrybeHotel.Services
         private readonly TokenOptions _tokenOptions;
         public TokenGenerator()
         {
-           _tokenOptions = new TokenOptions {
+            _tokenOptions = new TokenOptions
+            {
                 Secret = "4d82a63bbdc67c1e4784ed6587f3730c",
                 ExpiresDay = 1
-           };
+            };
 
         }
         public string Generate(UserDto user)
         {
-            throw new NotImplementedException();
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(AddClaims(user)),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(Encoding.ASCII.GetBytes(_tokenOptions.Secret)), SecurityAlgorithms.HmacSha256Signature),
+                Expires = DateTime.Now.AddDays(_tokenOptions.ExpiresDay)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            return tokenHandler.WriteToken(token);
         }
 
         private ClaimsIdentity AddClaims(UserDto user)
         {
-            throw new NotImplementedException();
+            ClaimsIdentity claimsIdentity = new ClaimsIdentity();
+
+            claimsIdentity.AddClaim(new Claim(ClaimTypes.Email, user.Email));
+
+            if (user.UserType == "admin")
+            {
+                claimsIdentity.AddClaim(new Claim(ClaimTypes.Role, "admin"));
+            }
+
+            return claimsIdentity;
         }
     }
 }
